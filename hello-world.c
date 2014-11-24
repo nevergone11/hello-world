378e868ac0535c0cac5f173b3332cb86a3535df4
nevergone11 <nevergone11@users.noreply.github.com>
2014-11-24 21:20:42 +0800

    Create hello-world.c
    printf hello world

---
+++ hello-world.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright(C) 2014 nevergone. All rights reserved.
+ */
+/*
+ * hello-world.c
+ * Original Author:  xing8788@gmail.com, 2014-11-22
+ *
+ * Just printf a log
+ *
+ * History
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv)
+{
+    printf("Hello World\n");
+
+    return 0;
+}
